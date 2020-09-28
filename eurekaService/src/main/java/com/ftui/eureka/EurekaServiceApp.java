a7dae235d770325ed4d5c0cd7c1e0d689c102c82
chaogaoxiaojidefantong <759646095@qq.com>
2020-09-28 18:33:08 +0800

    log添加打印的位置

--- eurekaService/src/main/java/com/ftui/eureka/EurekaServiceApp.java
+++ eurekaService/src/main/java/com/ftui/eureka/EurekaServiceApp.java
@@ -1,12 +1,17 @@
 package com.ftui.eureka;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
 @EnableEurekaServer
 public class EurekaServiceApp {
+    public  static  Logger logger = LoggerFactory.getLogger(EurekaServiceApp.class);
     public static void main(String[] args) {
+        logger.error("xixi");
+        logger.info("gggg");
         SpringApplication.run(EurekaServiceApp.class,args);
     }
 }
